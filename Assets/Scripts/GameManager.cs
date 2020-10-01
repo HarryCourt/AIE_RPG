a95746e38eb61f5ec79ae2b3e7d8131c4413ff95
Harry Court <harrison.court25@gmail.com>
2020-10-01 23:35:08 +0930

    Update
    + Added Main Menu
    + Added "Humble" music
    ~ Adjusted Wooden material

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public bool isStartMenu;    // Is the start of the scene in a menu?
+
     public enum GameState {Menu, Playing, GameOver}
 
     // Start is called before the first frame update
     void Start()
     {
-        // Hide cursor.
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = true;
+        if (isStartMenu) { return; }
+        else 
+        {
+            // Hide cursor.
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = true;
+        }
     }
     // Update is called once per frame
     void Update()
